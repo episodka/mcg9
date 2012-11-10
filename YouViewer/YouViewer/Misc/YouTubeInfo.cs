d31e1b0e7b6464239e7cb19a597077269111275d
ohtehands <ohtehands@gmail.com>
2012-11-10 15:51:52 +0000

    - Add function: history, bookmark

---
+++ YouViewer/YouViewer/Misc/YouTubeInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YouViewer
+{
+    /// <summary>
+    /// A simple data class
+    /// </summary>
+    public class YouTubeInfo
+    {
+        #region Data
+        public string LinkUrl { get; set; }
+        public string EmbedUrl { get; set; }
+        public string ThumbNailUrl { get; set; }
+        public string imgOffLink { get; set; }
+        #endregion
+    }
+}
