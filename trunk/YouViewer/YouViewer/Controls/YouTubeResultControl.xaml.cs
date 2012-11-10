747e1cc4bad7e0c718b85205c64182ae253e9128
ohtehands@gmail.com <ohtehands@gmail.com@9e883f26-8e72-8ca5-0cc4-2c372fe0cb68>
2012-11-10 15:51:52 +0000

    - Add function: history, bookmark

---
+++ trunk/YouViewer/YouViewer/Controls/YouTubeResultControl.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.IO;
+using System.Security.Cryptography;
+using WPF.JoshSmith.Controls;
+
+namespace YouViewer
+{
+
+    public enum SelectedMode { DRAG, PLAY, PLAYLIST, BOOKMARK, IDLE };
+    /// <summary>
+    /// Event delegate
+    /// </summary>
+    public delegate void SelectedEventHandler(object sender, YouTubeResultEventArgs e);
+
+
+    /// <summary>
+    /// Shows a single image, and when Mouse is over and the mode is
+    /// not in drag mode, then show a play button, which when
+    /// clicked will notify the YouViewerMainWindow to show a
+    /// Viewer control
+    /// </summary>
+    public partial class YouTubeResultControl : UserControl
+    {
+        #region Data
+        private SelectedMode currentMode;
+        private bool dragMode = true;
+        private YouTubeInfo info = null;
+
+        public event SelectedEventHandler SelectedEvent;
+        #endregion
+
+        #region Ctor
+        public YouTubeResultControl()
+        {
+
+            InitializeComponent();
+            //Loaded
+            this.Loaded += delegate
+            {
+                CurrentMode = SelectedMode.DRAG;
+                imageMain.SetValue(DragCanvas.CanBeDraggedProperty, true);
+            };
+            //MouseEnter
+            this.MouseEnter += delegate
+            {
+                if (CurrentMode == SelectedMode.PLAY)
+                {
+                    Storyboard sb = this.TryFindResource("OnMouseEnter") as Storyboard;
+                    if (sb != null)
+                        sb.Begin(this);
+                }
+            };
+            //MouseLeave
+            this.MouseLeave += delegate
+            {
+                if (CurrentMode == SelectedMode.PLAY)
+                {
+                    Storyboard sb = this.TryFindResource("OnMouseLeave") as Storyboard;
+                    if (sb != null)
+                        sb.Begin(this);
+                }
+            };
+        }
+        #endregion
+
+        #region Events
+        /// <summary>
+        /// Raised when this control btnPlay is clicked
+        /// </summary>
+        protected virtual void OnSelectedEvent(YouTubeResultEventArgs e) 
+        {
+            if (SelectedEvent != null) 
+            {
+                //Invokes the delegates.
+                SelectedEvent(this, e); 
+            }
+        }
+        #endregion
+
+        #region Properties
+
+        private string ImageUrl
+        {
+            set
+            {
+             //   String photolocation = YouViewerMainWindow.image_path + "\\" + YouViewerMainWindow.CalculateMD5Hash(Info.LinkUrl) + ".bmp";  //file name 
+             //   FileInfo file = new FileInfo(photolocation);
+              //  if (!file.Exists)
+             //   {
+                    BitmapImage bmp = new BitmapImage(new Uri(value, UriKind.RelativeOrAbsolute));
+                //    FileStream stream = new FileStream(photolocation, FileMode.Create);
+               //     TiffBitmapEncoder encoder = new TiffBitmapEncoder();
+               //     encoder.Frames.Add(BitmapFrame.Create(bmp));
+               //     encoder.Save(stream);
+                    imageMain.Source = bmp;
+              /*  }
+                else
+                {
+                    ImageSource imageSource = new BitmapImage(new Uri(photolocation));
+                    imageMain.Source = imageSource;
+                }*/
+            }
+        }
+
+
+        public YouTubeInfo Info
+        {
+            get { return info; }
+            set
+            {
+                info = value;
+                ImageUrl = info.ThumbNailUrl;
+            }
+        }
+
+        public SelectedMode CurrentMode
+        {
+            get {return currentMode;}
+            set
+            {
+                currentMode = value;
+                imageMain.SetValue(DragCanvas.CanBeDraggedProperty, dragMode);
+                string title = "";
+                switch (currentMode)
+                {
+                    case SelectedMode.DRAG:
+                        title = "Drag Mode";
+                        break;
+                    case SelectedMode.PLAY:
+                        title = "Play Mode";
+                        break;
+                    case SelectedMode.PLAYLIST:
+                        title = "Playlist Mode";
+                        break;
+                    case SelectedMode.IDLE:
+                        title = "";
+                        break;
+                    default:
+                        title = "Bookmark Mode";
+                        break;
+                }
+                lblDragMode.Content = title;
+            }
+        }
+        
+        #endregion
+
+        #region Private Methods
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentMode == SelectedMode.PLAY)
+            {
+                OnSelectedEvent(new YouTubeResultEventArgs(Info));
+                string filename = YouViewerMainWindow.history_path + "\\" + DateTime.Now.Year + "\\" + DateTime.Now.Month;
+                DirectoryInfo di = new DirectoryInfo(filename);
+                if (!di.Exists) di.Create();
+                File.AppendAllText(filename + "\\" + DateTime.Now.Day + ".htr", DateTime.Now.TimeOfDay.TotalSeconds + Environment.NewLine + Info.LinkUrl + Environment.NewLine + Info.EmbedUrl + Environment.NewLine + Info.ThumbNailUrl + Environment.NewLine);
+            }
+            else if (CurrentMode == SelectedMode.BOOKMARK)
+            {
+                string filename = YouViewerMainWindow.bookmark_path + "\\general.bmk";
+                File.AppendAllText(filename,Environment.NewLine + Info.LinkUrl + Environment.NewLine + Info.EmbedUrl + Environment.NewLine + Info.ThumbNailUrl + Environment.NewLine);
+            }
+        }
+        #endregion
+
+        private void menuAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuPlayState_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void menuAddPlaylist_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void menuSubFilmBookmark_Click(object sender, RoutedEventArgs e)
+        {
+            File.AppendAllText(YouViewerMainWindow.bookmark_path + "\\film.bmk", DateTime.Now.TimeOfDay.TotalSeconds + Environment.NewLine + Info.LinkUrl + Environment.NewLine + Info.EmbedUrl + Environment.NewLine + Info.ThumbNailUrl + Environment.NewLine);
+        }
+
+        private void menuSubMusicBookmark_Click(object sender, RoutedEventArgs e)
+        {
+            File.AppendAllText(YouViewerMainWindow.bookmark_path + "\\music.bmk", DateTime.Now.TimeOfDay.TotalSeconds + Environment.NewLine + Info.LinkUrl + Environment.NewLine + Info.EmbedUrl + Environment.NewLine + Info.ThumbNailUrl + Environment.NewLine);
+        }
+
+        private void menuSubEducationBookmark_Click(object sender, RoutedEventArgs e)
+        {
+            File.AppendAllText(YouViewerMainWindow.bookmark_path + "\\education.bmk", DateTime.Now.TimeOfDay.TotalSeconds + Environment.NewLine + Info.LinkUrl + Environment.NewLine + Info.EmbedUrl + Environment.NewLine + Info.ThumbNailUrl + Environment.NewLine);
+        }
+
+        private void menuSubTutorialBookmark_Click(object sender, RoutedEventArgs e)
+        {
+            File.AppendAllText(YouViewerMainWindow.bookmark_path + "\\tutorial.bmk", DateTime.Now.TimeOfDay.TotalSeconds + Environment.NewLine + Info.LinkUrl + Environment.NewLine + Info.EmbedUrl + Environment.NewLine + Info.ThumbNailUrl + Environment.NewLine);
+        }
+
+        private void menuSubOtherBookmark_Click(object sender, RoutedEventArgs e)
+        {
+            File.AppendAllText(YouViewerMainWindow.bookmark_path + "\\other.bmk", DateTime.Now.TimeOfDay.TotalSeconds + Environment.NewLine + Info.LinkUrl + Environment.NewLine + Info.EmbedUrl + Environment.NewLine + Info.ThumbNailUrl + Environment.NewLine);
+        }
+       
+    }
+}
---
+++ trunk/YouViewer/YouViewer/YouViewerMainWindow.xaml.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Runtime.Caching;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace YouViewer
+{
+    /// <summary>
+    /// Hosts a WPF.JoshSmith.Controls.DragCanvas which
+    /// in turn holds n-many YouTubeResultControls
+    /// </summary>
+    public partial class YouViewerMainWindow : Window
+    {
+        #region Data
+        private Random rand = new Random(50);
+        #endregion
+
+        #region Ctor
+        public static string app_path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static string bookmark_path = app_path + "\\bookmark";
+        public static string history_path = app_path + "\\history";
+        public static string image_path = app_path + "\\images";
+        public YouViewerMainWindow()
+        {
+            InitializeComponent();
+            viewer.ClosedEvent += viewer_ClosedEvent;
+        }
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Raised when Viewer is closed
+        /// </summary>
+        private void viewer_ClosedEvent(object sender, EventArgs e)
+        {
+            dragCanvas.Opacity = 1.0;
+        }
+
+        /// <summary>
+        /// Create a new YouTubeResultControl for each YouTubeInfo in input list
+        /// </summary>
+        private void PopulateCanvas(List<YouTubeInfo> infos, bool ordered)
+        {
+            dragCanvas.Children.Clear();
+            for (int i = 0; i < infos.Count; i++)
+            {
+                YouTubeResultControl control = new YouTubeResultControl { Info = infos[i] };
+                if (!ordered)
+                {
+                    int angleMutiplier = i % 2 == 0 ? 1 : -1;
+                    control.RenderTransform = new RotateTransform { Angle = GetRandom(30, angleMutiplier) };
+                    control.SetValue(Canvas.LeftProperty, GetRandomDist(dragCanvas.ActualWidth - 150.0));
+                    control.SetValue(Canvas.TopProperty, GetRandomDist(dragCanvas.ActualHeight - 150.0));
+                }
+                else
+                {
+                    control.SetValue(Canvas.LeftProperty, 20.0);
+                    control.SetValue(Canvas.TopProperty, 170.0 * i + 20.0);
+                }
+                control.SelectedEvent += control_SelectedEvent;
+                dragCanvas.Children.Add(control);
+            }
+        }
+
+        /// <summary>
+        /// Raised when a new YouTubeResultControl is selected
+        /// </summary>
+        private void control_SelectedEvent(object sender, YouTubeResultEventArgs e)
+        {
+            dragCanvas.Opacity = 0.6;
+            viewer.VideoUrl = e.Info.EmbedUrl;
+        }
+
+        /// <summary>
+        /// Get a new random number between 0.0 and 1.0 which is mutltiplied by
+        /// limit and angleMutiplier
+        /// </summary>
+        private int GetRandom(double limit, int angleMutiplier)
+        {
+            return (int)((rand.NextDouble() * limit) * angleMutiplier);
+        }
+
+        /// <summary>
+        /// Get a new random number between 0.0 and 1.0 which is mutltiplied by
+        /// limit
+        /// </summary>
+        private double GetRandomDist(double limit)
+        {
+            return rand.NextDouble() * limit;
+        }
+
+        /// <summary>
+        /// Do a key word search
+        /// </summary>
+        private void txtKeyWord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (txtKeyWord.Text != string.Empty)
+                {
+                    List<YouTubeInfo> infos = YouTubeProvider.LoadVideosKey(txtKeyWord.Text);
+                    PopulateCanvas(infos, false);
+                    ObjectCache cache = MemoryCache.Default;
+                    CacheItemPolicy policy = new CacheItemPolicy();
+                    policy.AbsoluteExpiration = DateTimeOffset.Now.AddDays(10.0);
+                    cache.Add("lastSearchResults", infos, policy, null);
+                }
+                else
+                {
+                    MessageBox.Show("you need to enter a search word", "Error",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+
+        #region Drag Methods
+        /// <summary>
+        /// Toggles the WPF.JoshSmith.Controls.DragCanvas.SetCanBeDragged
+        /// property for all the YouTubeResultControl controls within the
+        /// WPF.JoshSmith.Controls.DragCanvas
+        /// </summary>
+        #endregion
+
+
+        #endregion
+
+        private void menuLoadCache_Click(object sender, RoutedEventArgs e)
+        {
+            ObjectCache cache = MemoryCache.Default;
+            List<YouTubeInfo> cached = cache["lastSearchResults"] as List<YouTubeInfo>;
+            string msg = "Loading Cache: ";
+            if (cached == null) msg += " NULL";
+            else if (cached.Count > 0) msg += cached.Count + " elements";
+            else msg += " 0 element";
+            MessageBox.Show(msg);
+            if (cached != null) PopulateCanvas(cached, false);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            DirectoryInfo di = new DirectoryInfo(bookmark_path);
+            if (!di.Exists) di.Create();
+            DirectoryInfo dii = new DirectoryInfo(history_path);
+            if (!dii.Exists) dii.Create();
+            DirectoryInfo diii = new DirectoryInfo(image_path);
+            if (!diii.Exists) diii.Create();
+            /*
+            ObjectCache cache = MemoryCache.Default;
+            List<YouTubeInfo> cached = cache["lastSearchResults"] as List<YouTubeInfo>;
+            string msg = "Loading Cache: ";
+            if (cached == null) msg += " NULL";
+            else if (cached.Count > 0) msg += cached.Count + " elements";
+            else msg += " 0 element";
+            //  MessageBox.Show(msg);
+            if (cached != null) PopulateCanvas(cached, false);*/
+        }
+
+
+        private void btnHistoryView_Click(object sender, RoutedEventArgs e)
+        {
+
+            string filename = YouViewerMainWindow.history_path + "\\" + DateTime.Now.Year + "\\" + DateTime.Now.Month + "\\" + DateTime.Now.Day + ".htr";
+            List<YouTubeInfo> temp = readBookMarkFile(filename);
+            PopulateCanvas(temp, true);
+        }
+
+        public static List<YouTubeInfo> readBookMarkFile(string filename)
+        {
+            List<YouTubeInfo> infoHistory = new List<YouTubeInfo>();
+            infoHistory.Add(new YouTubeInfo());
+            infoHistory.Clear();
+            FileInfo file = new FileInfo(filename);
+            if (!file.Exists) return infoHistory;
+            StreamReader reader = file.OpenText();
+
+            reader.ReadLine();
+            string link = reader.ReadLine();
+            string embed = reader.ReadLine();
+            string thumb = reader.ReadLine();
+            while (link != null && embed != null && thumb != null)
+            {
+                YouTubeInfo yInfo = new YouTubeInfo();
+                yInfo.LinkUrl = link;
+                yInfo.EmbedUrl = embed;
+                yInfo.ThumbNailUrl = thumb;
+                infoHistory.Add(yInfo);
+                reader.ReadLine();
+                link = reader.ReadLine();
+                embed = reader.ReadLine();
+                thumb = reader.ReadLine();
+            }
+            return infoHistory;
+        }
+
+        public static string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            List<YouTubeInfo> list = new List<YouTubeInfo>();
+            PopulateCanvas(list, false);
+        }
+
+        private void btnMode_Click(object sender, RoutedEventArgs e)
+        {
+            if (dragCanvas.Children.Count == 0) return;
+            bool canBeDragged = WPF.JoshSmith.Controls.DragCanvas.GetCanBeDragged(dragCanvas.Children[0]);
+            canBeDragged = !canBeDragged;
+            this.btnMode.Content = canBeDragged ? "Play Mode" : "Drag Mode";
+            foreach (YouTubeResultControl child in dragCanvas.Children)
+            {
+                WPF.JoshSmith.Controls.DragCanvas.SetCanBeDragged(child, canBeDragged);
+                child.CurrentMode = canBeDragged ? SelectedMode.DRAG : SelectedMode.PLAY;
+            }
+        }
+
+        private void cbmBookmark_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            List<YouTubeInfo> list = new List<YouTubeInfo>();
+            switch (this.cbmBookmark.SelectedIndex)
+            {
+                case -1: return;
+                case 0:
+                    list = readBookMarkFile(bookmark_path + "\\film.bmk");
+                    break;
+                case 1:
+                    list = readBookMarkFile(bookmark_path + "\\music.bmk");
+                    break;
+                case 2:
+                    list = readBookMarkFile(bookmark_path + "\\tutorial.bmk");
+                    break;
+                case 3:
+                    list = readBookMarkFile(bookmark_path + "\\education.bmk");
+                    break;
+                default:
+                    list = readBookMarkFile(bookmark_path + "\\other.bmk");
+                    break;
+            }
+            PopulateCanvas(list, true);
+        }
+    }
+}
